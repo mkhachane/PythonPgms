56bc6c58afe38940a50395f827ae53a89f45667d
mkhachane <mayur.khachane@opcito.com>
2018-02-12 16:31:05 +0530

    ForExample updated

--- ForExample.py
+++ ForExample.py
@@ -3,12 +3,12 @@ num = int(input("Enter Number: "))
 for i in range(1,10+1):
     print i, "*", num ,"=",num*i
 
-# 2nd Example:
-num = int(input("Enter number: "))
+# 2nd Example like prime numbers upto  :
+num = int(input("Enter numbers upto : "))
 for num in range(1,num):
     if num > 1:
         for i in range(2,num):
             if (num % i) == 0:
                 break
         else:
-            print
+            print num
