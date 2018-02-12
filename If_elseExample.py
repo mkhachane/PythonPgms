f19881c77c743e949bb5d8391c13bcc182e4bda8
mkhachane <mayur.khachane@opcito.com>
2018-02-12 17:04:11 +0530

    If_elseExample is updated

--- If_elseExample.py
+++ If_elseExample.py
@@ -12,12 +12,13 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 num3 = int(input("Enter 3rd number: "))
 
-if num1 < num2:
-    largest = num2
-elif num2 < num3:
-    largest = num3
-elif num3 < num1:
+if (num1 > num2) and (num1 > num3):
     largest = num1
+elif (num2 > num1) and (num2 > num3):
+    largest = num2
+else:
+    largest = num3
+
 print "The largest number of ",num1,num2,"and",num3,"is",largest
 
 
