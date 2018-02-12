63cf54e79192cb3b0c1878c46aaba0d5fcebe736
mkhachane <mayur.khachane@opcito.com>
2018-02-12 14:33:11 +0530

    WhileExample Update

--- WhileExample.py
+++ WhileExample.py
@@ -1,4 +1,13 @@
-# While loop Example
+#Example for sum of natural number:
+num = int(input("Enter number: "))
+sum =0
+i =1
+while i <= num:
+    sum =sum + i
+    i += 1
+print "The sum of",num,"is",sum
+
+# While loop Example like febo
 nterms = int(input("Enter terms: "))
 n1 = 0
 n2 =1
@@ -9,12 +18,3 @@ while count <nterms:
     n2 = temp
     print temp
     count +=1
-
-#Example for sum of natural number:
-num = int(input("Enter number: "))
-sum =0
-i =1
-while i <= num:
-    sum =sum + i
-    i += 1
-print "The sum of",num,"is",sum
