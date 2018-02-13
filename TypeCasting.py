ba2d4a9e2eadd4973e22cb8a3fd71b7f4b191f10
mkhachane <mayur.khachane@opcito.com>
2018-02-13 12:17:46 +0530

    TypeCasting file is2nd time  uploaded

--- TypeCasting.py
+++ TypeCasting.py
@@ -5,11 +5,4 @@ except (ZeroDivisionError,TypeError):
     print "You can't devide by zero or string"
 
 
-try:
-   fh = open("testfile", "w")
-   fh.write("This is my test file for exception handling!!")
-except IOError:
-   print "Error: can\'t find file or read data"
-else:
-   print "Written content in the file successfully"
-   fh.close()
+
