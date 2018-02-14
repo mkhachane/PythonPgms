35fe888d9f6be7369ea54e3760e2e8b88e0340fb
mkhachane <mayur.khachane@opcito.com>
2018-02-15 02:06:50 +0530

    Completed recursion pgm

--- SearchKeyRecursive.py
+++ SearchKeyRecursive.py
@@ -1,19 +1,22 @@
 import urllib,json
 
-
-def recursive(data):
-    v = raw_input('Enter the key: ')
-    for key,value in data.items():
-        if key == v:
-            print value
-            if isinstance(value,list):
-                print 'yes'
-
-
-
+def _finditem(obj, key):
+    if key in obj:
+        return obj[key]
+    for k, v in obj.items():
+        if isinstance(v,dict):
+            return _finditem(v, key)
+        if isinstance(v,list):
+            for i in v:
+                if key in i:
+                    print _finditem(i,key)
+                # if isinstance(i, dict):
+                #     return _finditem(i, key)
 
 
 if __name__ == '__main__':
     link = "http://demo6920974.mockable.io/json-example"
     data = json.load(urllib.urlopen(link))
-    recursive(data)
+    v = raw_input('Enter the key: ')
+    d = _finditem(data, v)
+    print d
