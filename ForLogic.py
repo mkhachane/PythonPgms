a33594861caadb81f701f84f0c82fc86883cdb48
mkhachane <mayur.khachane@opcito.com>
2018-02-15 21:01:02 +0530

    File uploaded

---
+++ ForLogic.py
@@ -0,0 +1,77 @@
+n = input('Enter: ')
+for i in range(1, 6):
+    print ('*' * n)
+
+for i in range (n,0,-1):
+     print((n-i) * ' ' + i * '*')
+
+for i in range(n):
+    print ('*' * (i+1))
+
+for i in range(n):
+    print ((n-i) * '*')
+
+for i in range (n,0,-1):
+     print((i) * ' ' + (n+1-i) * '*')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
