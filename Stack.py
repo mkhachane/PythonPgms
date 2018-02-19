6f103fc96907217ec9bd8c838ad4796905ed5dbd
mkhachane <mayur.khachane@opcito.com>
2018-02-19 10:09:38 +0530

    Complete Stack pgm

--- Stack.py
+++ Stack.py
@@ -1,21 +1,30 @@
+
 class Stack:
     def __init__(self):
         self.mStack = []
+        self.maxSize = 5
+        self.top = 0
 
     def isEmpty(self):
         return self.mStack == []
 
 
     def push(self, item):
-        self.mStack.append(item)
-        print "Inserted item: %d"% int(item)
+        if self.top >= self.maxSize:
+            print "Stack is full"
+        else:
+            self.mStack.append(item)
+            self.top += 1
+            print "Inserted item: %d"% int(item)
 
     def pop(self):
-        if self.isEmpty() == False:
-            item = self.mStack.pop()
-            print "Removed item : %d"% int(item)
+        if self.top <= 0:
+            print "Stack is Empty"
         else:
-            print "No item in stack"
+            item = self.mStack.pop()
+            self.top -= 1
+            print "Removed item : %d" % int(item)
+
 
     def display(self):
         if self.isEmpty() == False:
